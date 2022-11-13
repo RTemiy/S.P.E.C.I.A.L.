b1a592bc8384d928ace9bd61ab5fd5420efa7181
RTemiy <83100656+RTemiy@users.noreply.github.com>
2022-11-13 19:28:49 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+class Char {
+    constructor(a) {
+        this.name = a.name;
+        this.icon = a.icon;
+        this.value = 0;
+        this.Build();
+    }
+    Build() {
+        this.containerEl = document.createElement('container');
+        this.iconEl = document.createElement('icon');
+        this.iconEl.innerText = this.icon;
+        this.nameEl = document.createElement('a');
+        this.nameEl.innerHTML = '⠀' + this.name;
+        this.valueEl = document.createElement('av');
+        this.increaseEl = document.createElement('button');
+        this.increaseEl.innerHTML = '+';
+        this.increaseEl.onclick = () => { this.ChangeValue(1); }
+        this.decreaseEl = document.createElement('button');
+        this.decreaseEl.innerHTML = '-';
+        this.decreaseEl.onclick = () => { this.ChangeValue(-1); }
+
+        this.containerEl.appendChild(this.iconEl);
+        this.containerEl.appendChild(this.nameEl);
+        this.containerEl.appendChild(this.decreaseEl);
+        this.containerEl.appendChild(this.increaseEl);
+        this.containerEl.appendChild(this.valueEl);
+
+        document.body.appendChild(this.containerEl);
+        this.valueEl.innerHTML = this.value + '⠀';
+    }
+    ChangeValue(b) {
+        if(this.value<= 0 && b<=0){}
+        else if (this.value>= 10 && b>=0){}
+        else{
+        this.value += b;
+        this.valueEl.innerHTML = this.value + '⠀';
+        PlayerLevel.ChangeLevel(b);
+        }
+    }
+
+}
+
+class Level{
+    constructor(){
+        this.m = document.createElement("p");
+        this.value = 0;
+        document.body.appendChild(this.m);
+        this.ChangeLevel(0);
+    }
+    ChangeLevel(a){
+        this.value+=a;
+        this.m.innerHTML="Уровень: "+this.value;
+    }
+}
+
+Characters = {}
+
+PlayerLevel = new Level();
+
+Characters.Strength = new Char({
+    name: 'Сила',
+    icon: '💪',
+});
+
+Characters.Perception = new Char({
+    name: 'Восприятие',
+    icon: '👁',
+});
+
+Characters.Endurance = new Char({
+    name: 'Выносливость',
+    icon: '🏃',
+});
+
+Characters.Charisma = new Char({
+    name: 'Харизма',
+    icon: '🙇‍♂️',
+});
+
+Characters.Intelligence = new Char({
+    name: 'Интеллект',
+    icon: '🧠',
+});
+
+Characters.Agility = new Char({
+    name: 'Ловкость',
+    icon: '🤸‍♂️',
+});
+
+Characters.Luck = new Char({
+    name: 'Удача',
+    icon: '🍀',
+});
